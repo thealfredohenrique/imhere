4e140767a0758cbd4265807642b3dccdf14920e4
Alfredo Henrique <thealfredohenrique@gmail.com>
2024-02-14 21:49:26 -0300

    feat: create home screen
    * Added a new home screen to the app.
    * The home screen provides a central location for users to access the app's main features.

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>
+        Quarta-feira, 14 de fevereiro de 2024.
+      </Text>
+    </View>
+  );
+}
